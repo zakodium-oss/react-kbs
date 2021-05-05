be173c3d9b29cf5ae484b874c67fc08afad6a64d
Michaël Zasso <targos@protonmail.com>
2021-05-05 15:37:32 +0200

    feat: warn if a global shortcut is defined twice

--- src/component/utils/combineShortcuts.ts
+++ src/component/utils/combineShortcuts.ts
@@ -10,6 +10,12 @@ export function combineShortcuts(
     for (const shortcut of shortcuts) {
       const keys = shortcutToKeys(shortcut);
       for (const key of keys) {
+        if (result[key] !== undefined) {
+          // eslint-disable-next-line no-console
+          console.warn(
+            `A global shortcut was already defined for key the combination ${key}. It will be overriden.`,
+          );
+        }
         result[key] = shortcut;
       }
     }

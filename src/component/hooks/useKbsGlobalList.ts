b79ad3de3a2f7f9295d82b34d1f9bf15ca2b4b53
Michaël Zasso <targos@protonmail.com>
2021-05-06 10:14:55 +0200

    fix: include typings in distribution

--- src/component/hooks/useKbsGlobalList.ts
+++ src/component/hooks/useKbsGlobalList.ts
@@ -3,6 +3,6 @@ import { useContext } from 'react';
 import { kbsContext } from '../KbsProvider';
 
 // TODO: wip
-export function useKbsGlobalList() {
+export function useKbsGlobalList(): any {
   return useContext(kbsContext);
 }
